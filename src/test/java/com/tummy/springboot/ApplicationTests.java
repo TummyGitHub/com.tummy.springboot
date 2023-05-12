9822f8f3a49c7a0c7eb81809fcc2fa6555c24469
tummy <2969973284@qq.com>
2023-05-12 20:46:55 +0800

    Initial commit

---
+++ src/test/java/com/tummy/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tummy.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
